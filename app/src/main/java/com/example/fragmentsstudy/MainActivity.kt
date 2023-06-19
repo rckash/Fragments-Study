b582245dfaf13c7948a0e1350c2d7414d2ed164b
RC <rckashyapcpe@gmail.com>
2023-06-19 11:53:16 +0800

    finished study

--- app/src/main/java/com/example/fragmentsstudy/MainActivity.kt
+++ app/src/main/java/com/example/fragmentsstudy/MainActivity.kt
@@ -15,6 +15,36 @@ class MainActivity : AppCompatActivity() {
         //create objects from Fragments
         val f1 = FragmentOne()
         val f2 = FragmentTwo()
+        val f3 = FragmentThree()
+
+        //default fragment
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragmentContainerView, f1)
+            commit()
+        }
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.menu_home -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainerView, f1)
+                        commit()
+                    }
+                }
+                R.id.menu_profile -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainerView, f2)
+                        commit()
+                    }
+                }
+                R.id.menu_notification -> {
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainerView, f3)
+                        commit()
+                    }
+                }
+            }
+            true
+        }
 
         binding.btnFragmentOne.setOnClickListener {
             supportFragmentManager.beginTransaction().apply {
